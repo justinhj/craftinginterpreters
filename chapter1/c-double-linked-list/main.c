bb768ae4464a5ae1b9ae072c75aa8dfeb4850e5f
Justin Heyes-Jones <justinhj@gmail.com>
2022-04-30 21:18:41 -0700

    remove superfuous

--- chapter1/c-double-linked-list/main.c
+++ chapter1/c-double-linked-list/main.c
@@ -18,36 +18,3 @@ int main(void) {
 
     print_list(head);
 }
-
-void tests() {
-    Element *head = NULL;  
-    head = insert(head, "Hello");
-    head = insert(head, "World");
-    head = insert(head, "How");
-    head = insert(head, "are");
-
-    printf("Found Hello? %c\n", find(head, "Hello") ? 'Y' : 'N');
-    printf("Found How? %c\n", find(head, "How") ? 'Y' : 'N');
-    printf("Found you?? %c\n", find(head, "you?") ? 'Y' : 'N');
-
-    head = insert(head, "you?");
-    printf("Found you?? %c\n", find(head, "you?") ? 'Y' : 'N');
-
-    print_list(head);
-
-    // remove_if_found first 
-    head = remove_if_found(head, "Hello");
-    printf("Found Hello? %c\n", find(head, "Hello") ? 'Y' : 'N');
-
-    print_list(head);
-    // remove_if_found last
-    head = remove_if_found(head, "you?");
-    printf("Found you?? %c\n", find(head, "you?") ? 'Y' : 'N');
-
-    print_list(head);
-    // remove_if_found middle
-    head = remove_if_found(head, "are");
-    printf("Found are? %c\n", find(head, "are") ? 'Y' : 'N');
-
-    print_list(head);
-}

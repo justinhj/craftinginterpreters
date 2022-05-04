5118a750a04b6e8828dbed000f48dbff4cf1a84c
Justin Heyes-Jones <justinhj@gmail.com>
2022-05-03 18:22:03 -0700

    tests running

--- chapter4/jli/src/main.rs
+++ chapter4/jli/src/main.rs
@@ -1,15 +1,29 @@
 // The library code is really just a scanner and contains not code load files and so on. This
 // executable handles the loading of scripts or scanning from input.
+use std::env;
+use std::fs;
 
 fn main() {
-    let input = "\
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() > 0 {
+        let source = fs::read_to_string(&args[0]).unwrap();
+
+        match jli::scan(&source) {
+            Ok(tokens) => tokens.iter().for_each(|token| println!("{:?}", token)),
+            Err(err) => println!("Error {:?}", err),
+        }
+
+    } else {
+        let input = "\
             fun addPair(a, b) {\n\
               c = 3.14;\n\
               return a + b * c;\n\
             }";
 
-    match jli::scan(input) {
-        Ok(tokens) => tokens.iter().for_each(|token| println!("{:?}", token)),
-        Err(err) => println!("Error {:?}", err),
+        match jli::scan(input) {
+            Ok(tokens) => tokens.iter().for_each(|token| println!("{:?}", token)),
+            Err(err) => println!("Error {:?}", err),
+        }
+        println!("Pass a filename")
     }
 }
